04aea97a726d2f1c01a5a49ec32be596346a017c
Son Nguyen <SonNTHE181860@fpt.edu.vn>
2026-03-20 15:45:28 +0700

    test pr analyzer 1st

--- src/routes/items.js
+++ src/routes/items.js
@@ -3,8 +3,8 @@ const itemController = require('../controllers/itemController');
 
 const router = express.Router();
 
-router.get('/', itemController.getList);    // CMUC 1
-router.get('/:id', itemController.getDetail);  // CMUC 3
+// router.get('/', itemController.getList);    // CMUC 1
+// router.get('/:id', itemController.getDetail);  // CMUC 3
 router.post('/', itemController.createItem); // CMUC 2
 router.put('/:id', itemController.updateItem); // CMUC 4
 router.delete('/:id', itemController.deleteItem); // CMUC 5
