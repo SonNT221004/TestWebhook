fb9867562fbda83ec50cfd9ea17938888017bca7
Son Nguyen <SonNTHE181860@fpt.edu.vn>
2026-03-20 15:39:03 +0700

    update route

--- src/routes/items.js
+++ src/routes/items.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', itemController.getList);    // CMUC 1
 router.get('/:id', itemController.getDetail);  // CMUC 3
 router.post('/', itemController.createItem); // CMUC 2
-router.put('/:id', itemController.updateItem); // CMUC 4
-router.delete('/:id', itemController.deleteItem); // CMUC 5
+//router.put('/:id', itemController.updateItem); // CMUC 4
+//router.delete('/:id', itemController.deleteItem); // CMUC 5
 
 module.exports = router;
